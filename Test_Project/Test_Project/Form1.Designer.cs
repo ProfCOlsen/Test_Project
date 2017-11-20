fa812a588326fb811a6544c53b6b4c374cbaf775
TeddySteady <clolsen2@asu.edu>
2017-11-20 07:19:30 -0700

    Added button to display Teddy's Bio form.

--- Test_Project/Test_Project/Form1.Designer.cs
+++ Test_Project/Test_Project/Form1.Designer.cs
@@ -29,24 +29,38 @@
         private void InitializeComponent()
         {
             this.professorButton = new System.Windows.Forms.Button();
+            this.teddyButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // professorButton
             // 
-            this.professorButton.Location = new System.Drawing.Point(33, 32);
+            this.professorButton.Location = new System.Drawing.Point(22, 21);
+            this.professorButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.professorButton.Name = "professorButton";
-            this.professorButton.Size = new System.Drawing.Size(126, 29);
+            this.professorButton.Size = new System.Drawing.Size(84, 23);
             this.professorButton.TabIndex = 0;
             this.professorButton.Text = "Professor Says";
             this.professorButton.UseVisualStyleBackColor = true;
             this.professorButton.Click += new System.EventHandler(this.professorButton_Click);
             // 
+            // teddyButton
+            // 
+            this.teddyButton.Location = new System.Drawing.Point(121, 21);
+            this.teddyButton.Margin = new System.Windows.Forms.Padding(2);
+            this.teddyButton.Name = "teddyButton";
+            this.teddyButton.Size = new System.Drawing.Size(84, 23);
+            this.teddyButton.TabIndex = 1;
+            this.teddyButton.Text = "Teddy";
+            this.teddyButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 540);
+            this.ClientSize = new System.Drawing.Size(513, 351);
+            this.Controls.Add(this.teddyButton);
             this.Controls.Add(this.professorButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Button professorButton;
+        private System.Windows.Forms.Button teddyButton;
     }
 }
 
