4adc2bf6f9a7fa66439991e11da88546b355bf6b
Christopher Olsen <clolsen2@asurite.asu.edu>
2017-11-20 07:37:02 -0700

    Removed Event Handler to Demonstrate two people committing changes at the same time.

--- Test_Project/Test_Project/Form1.Designer.cs
+++ Test_Project/Test_Project/Form1.Designer.cs
@@ -34,33 +34,29 @@
             // 
             // professorButton
             // 
-            this.professorButton.Location = new System.Drawing.Point(22, 21);
-            this.professorButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.professorButton.Location = new System.Drawing.Point(33, 32);
             this.professorButton.Name = "professorButton";
-            this.professorButton.Size = new System.Drawing.Size(84, 23);
+            this.professorButton.Size = new System.Drawing.Size(126, 35);
             this.professorButton.TabIndex = 0;
-            this.professorButton.Text = "Professor Says";
+            this.professorButton.Text = "Professor";
             this.professorButton.UseVisualStyleBackColor = true;
-            this.professorButton.Click += new System.EventHandler(this.professorButton_Click);
             // 
             // teddyButton
             // 
-            this.teddyButton.Location = new System.Drawing.Point(121, 21);
-            this.teddyButton.Margin = new System.Windows.Forms.Padding(2);
+            this.teddyButton.Location = new System.Drawing.Point(182, 32);
             this.teddyButton.Name = "teddyButton";
-            this.teddyButton.Size = new System.Drawing.Size(84, 23);
+            this.teddyButton.Size = new System.Drawing.Size(126, 35);
             this.teddyButton.TabIndex = 1;
             this.teddyButton.Text = "Teddy";
             this.teddyButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 351);
+            this.ClientSize = new System.Drawing.Size(770, 540);
             this.Controls.Add(this.teddyButton);
             this.Controls.Add(this.professorButton);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
